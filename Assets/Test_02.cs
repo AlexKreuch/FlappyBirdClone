a50d4d84d7f362d5effa1c691833cc0c1957f029
alexk <alexk@LAPTOP-518CEKNS>
2019-09-05 10:27:07 -0700

    added testing code

---
+++ Assets/Test_02.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Animations;
+
+public class Test_02 : MonoBehaviour
+{
+
+    private const string controllerName = "BlueBirdFlapping_controller";
+
+    private void btnMech(ref bool btn, System.Action action)
+    {
+        if (btn)
+        {
+            btn = false;
+            action();
+        }
+    }
+
+   
+
+    private void SetUp()
+    {
+        Animator anim = gameObject.AddComponent<Animator>();
+        anim.runtimeAnimatorController = Resources.Load<AnimatorController>(controllerName);
+
+        var sr = gameObject.AddComponent<SpriteRenderer>();
+    }
+    void Start() { SetUp(); }
+
+   
+}
