cc9f2f99c6434aa934f3066d107e794b6226bf7d
alexk <alexk@LAPTOP-518CEKNS>
2019-09-21 11:32:35 -0700

    Building PausePanel;

---
+++ Assets/HELPER002.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class HELPER002 : MonoBehaviour
+{
+    public Vector2Int screenSize = new Vector2Int();
+    void MaintainScreenSize()
+    {
+        screenSize = new Vector2Int(Screen.width,Screen.height);
+    }
+    void Update()
+    {
+        MaintainScreenSize();
+    }
+}
